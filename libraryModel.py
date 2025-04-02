dba394c5daa16b2e1aabb386348fb5bed25faa18
Temka0994 <pleskovartem878@gmail.com>
2025-04-02 21:06:20 +0300

    updated HistoryModel and added GET request to getting book history

--- libraryModel.py
+++ libraryModel.py
@@ -76,3 +76,4 @@ class HistoryModel(Base):
     book_id: Mapped[int] = mapped_column(ForeignKey("books.book_id", ondelete="CASCADE"))
     user_id: Mapped[int] = mapped_column(ForeignKey("users.user_id", ondelete="CASCADE"))
     operation: Mapped[str] = mapped_column(String(50))
+    operation_date: Mapped[date] = mapped_column(Date)
